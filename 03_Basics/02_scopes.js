5343c9faf04b71dce6947c20259d2f277616c980
Vandana200520 <vandanavandana91164@gmail.com>
2024-04-13 22:47:02 +0530

    scopes

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -1,22 +1,60 @@
 // Local and Global scope
 // var is used globally
 
-let a = 50
-const b = 5
-var c = 8
+//var c = 300
+let a = 5
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
 
-if(true){
+function one(){
+    const username = "vandana"
 
-    let a = 100
-    const b = 10
-    var c = 16
-    console.log("Inner: ",a);
-    console.log("Inner: ",b);
-    console.log("Inner: ",c);
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "vandana"
+    if (username === "vandana") {
+        const website = " youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+// it Will not throw error if used first and declared later
+
+console.log(addone(5))
+function addone(num){
+    return num + 1
 }
 
 
-console.log(a);
-console.log(b);
-console.log(c);     
+// Will throw error if used first and declared later(because a variable holds this function)
+
+// addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}   
