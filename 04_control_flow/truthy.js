9c2d1b691228fea8be818e42b3f26dfa9bcd62b6
Vandana200520 <157909108+Vandana200520@users.noreply.github.com>
2024-04-21 21:08:21 +0530

    truthy.js

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -32,17 +32,15 @@ if (Object.keys(emptyObj).length === 0) {
 // Nullish Coalescing Operator (??): null undefined
 
 let val1;
-// val1 = 5 ?? 10
-// val1 = null ?? 10
+// val1 = 5 ?? 10          //first number will be output if both are number
+// val1 = null ?? 10       // output will be number
 // val1 = undefined ?? 15
 val1 = null ?? 10 ?? 20
-
-
 
 console.log(val1);
 
 // Terniary Operator(?)
-// condition ? true : false
+// condition ? true : false    (syntax)
 
 const iceTeaPrice = 100
 iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
